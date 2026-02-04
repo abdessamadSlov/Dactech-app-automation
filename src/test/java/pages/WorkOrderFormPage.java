9a57b0bb85c059e7c2e56e52ef8ccb811d863631
abdessamadSlov <abdessamad.badouch@gmail.com>
2026-02-04 18:18:26 +0100

    Update WorkOrderFormPage.java

--- src/test/java/pages/WorkOrderFormPage.java
+++ src/test/java/pages/WorkOrderFormPage.java
@@ -1,23 +1,14 @@
 package pages;
 
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import org.openqa.selenium.WebElement;
-
 public class WorkOrderFormPage extends BasePage {
 
-    @AndroidFindBy(id = "input_comment")
-    WebElement comment;
-
-    @AndroidFindBy(id = "checkbox_completed")
-    WebElement completed;
-
-    @AndroidFindBy(id = "btn_submit_ot")
-    WebElement submit;
-
     public void fillWorkOrder() {
-        comment.sendKeys("Intervention réalisée avec succès");
-        completed.click();
-        swipeUp();
-        submit.click();
+        find("workorder.comment").sendKeys("Intervention réalisée avec succès");
+        find("workorder.completed").click();
+
+        // Scroll jusqu’au bouton si besoin
+        scrollToElement(find("workorder.submit"));
+
+        find("workorder.submit").click();
     }
 }
