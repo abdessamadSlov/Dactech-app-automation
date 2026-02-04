8f33ea6ea465351f99e8dfc78e0782c6041f3bec
abdessamadSlov <abdessamad.badouch@gmail.com>
2026-02-04 18:15:54 +0100

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,26 +1,10 @@
 package pages;
 
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.pagefactory.iOSXCUITFindBy;
-import org.openqa.selenium.WebElement;
-
 public class LoginPage extends BasePage {
 
-    @AndroidFindBy(id = "input_username")
-    @iOSXCUITFindBy(accessibility = "input_username")
-    private WebElement username;
-
-    @AndroidFindBy(id = "input_password")
-    @iOSXCUITFindBy(accessibility = "input_password")
-    private WebElement password;
-
-    @AndroidFindBy(id = "btn_login")
-    @iOSXCUITFindBy(accessibility = "btn_login")
-    private WebElement loginBtn;
-
     public void authenticate(String user, String pwd) {
-        username.sendKeys(user);
-        password.sendKeys(pwd);
-        loginBtn.click();
+        find("login.username").sendKeys(user);
+        find("login.password").sendKeys(pwd);
+        find("login.loginBtn").click();
     }
 }
