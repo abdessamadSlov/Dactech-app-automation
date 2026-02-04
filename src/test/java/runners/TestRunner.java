b71bae86649026723e280b553d30c598705b0701
abdessamadSlov <abdessamad.badouch@gmail.com>
2026-02-04 18:19:29 +0100

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,14 +1,25 @@
 package runners;
 
+import config.ConfigReader;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeClass;
 
 @CucumberOptions(
     features = "src/test/resources/features",
-    glue = "steps"
+    glue = "steps",
+    plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    @Parameters({"deviceKey"})
+    public void setup(String deviceKey) {
+        // Initialise le driver pour la clé device passée
+        config.DriverFactory.initDriver(deviceKey);
+    }
 
     @Override
     @DataProvider(parallel = true)
