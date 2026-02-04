8a42a5786ad33241aa15b35f778c95dfaffaca50
abdessamadSlov <abdessamad.badouch@gmail.com>
2026-02-04 18:09:18 +0100

    Update SignaturePage.java

--- src/test/java/pages/SignaturePage.java
+++ src/test/java/pages/SignaturePage.java
@@ -1,27 +1,45 @@
 package pages;
 
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.pagefactory.AndroidFindBy;
-import io.appium.java_client.touch.offset.PointOption;
-import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 public class SignaturePage extends BasePage {
 
     @AndroidFindBy(id = "signature_pad")
-    WebElement signaturePad;
+    @iOSXCUITFindBy(accessibility = "signature_pad")
+    private WebElement signaturePad;
 
     @AndroidFindBy(id = "btn_validate_signature")
-    WebElement validate;
+    @iOSXCUITFindBy(accessibility = "btn_validate_signature")
+    private WebElement validate;
 
+
+
+
+
+
+    
     public void signByClient() {
-        Rectangle r = signaturePad.getRect();
+        int centerX = signaturePad.getRect().x + signaturePad.getRect().width / 2;
+        int centerY = signaturePad.getRect().y + signaturePad.getRect().height / 2;
 
-        new TouchAction<>(driver)
-            .press(PointOption.point(r.x + 10, r.y + 10))
-            .moveTo(PointOption.point(r.x + r.width - 10, r.y + r.height - 10))
-            .release()
-            .perform();
+        // W3C Actions : draw a simple line
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 1)
+            .addAction(finger.createPointerMove(Duration.ZERO,
+                    PointerInput.Origin.viewport(), centerX - 50, centerY))
+            .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+            .addAction(finger.createPointerMove(Duration.ofMillis(300),
+                    PointerInput.Origin.viewport(), centerX + 50, centerY))
+            .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        driver.perform(Arrays.asList(swipe));
 
         validate.click();
     }
