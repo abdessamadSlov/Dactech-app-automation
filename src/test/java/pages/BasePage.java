774d605ba95208981e18ab91b26bfc4450c2fb43
abdessamadSlov <abdessamad.badouch@gmail.com>
2026-02-05 12:31:41 +0100

    Create BasePage.java

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+public abstract class BasePage {
+
+    protected AppiumDriver driver;
+
+    public BasePage(AppiumDriver driver) {
+        this.driver = driver;
+    }
+
+    protected MobileElement find(String key) {
+        return driver.findElement(getLocator(key));
+    }
+
+}
